bd5302f2092280db36bd95d8ca32b2fcf3fb3fbf
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2023-01-03 20:06:10 +0100

    nog verbeteringen

--- src/components/Kansarmoede.jsx
+++ src/components/Kansarmoede.jsx
@@ -19,9 +19,13 @@ export default function Kansarmoede() {
             <p>Het is gebleken dat kansarme kinderen het zwaarder hebben op school. Doordat hun thuissituatie veel van hen vraagt, presteren ze minder goed op school. Ze schamen zich en zijn onzeker waardoor ze zich afsluiten van de buitenwereld (Help ons kinderarmoede bestrijden - Stichting Pelicano, z.d.).</p>
             <p>In armoede leven is iets wat iedereen kan overkomen. Er zijn wel verschillende groepen huishoudens die hier meer risico op lopen. Deze huishoudens hebben een relatief laag inkomen. Voorbeelden hiervan zijn: éénoudergezinnen, huishoudens met een migratieachtergrond, een laag opleidingsniveau en weinig werkervaring. Kinderen van huishoudens met deze kenmerken hebben vervolgens ook een hogere kans op armoede (Duurzame aanpak van kinderarmoede, 2022).  </p>
 
-            <p class="text-primary">In deze podcast <a href="https://ondermamas.be/podcast/kinderarmoede/">(Klik hier)</a> vertelt ‘Stichting Pelicano’ over het initiatief om elk kind een mooie en warme toekomst te kunnen geven.</p>
+            <p class="text-primary">In volgende podcast vertelt ‘Stichting Pelicano’ over het initiatief om elk kind een mooie en warme toekomst te kunnen geven.</p>
+            <a href="https://ondermamas.be/podcast/kinderarmoede/">
+                <button className='btn btn-primary mb-4'>podcast 'Stichting Pelicano'</button>
+            </a>
 
-            <img src={grafiek} alt='grafiekkansarmoede'></img>
+            <img className='imgCentered imgGrafiek' src={grafiek} alt='grafiekkansarmoede'></img>
+            <p>Vlaanderen. (z.d.-a). Armoede indicatoren op kind niveau. https://www.opgroeien.be/cijfers-en-publicaties/gezinsinkomen-en-kansarmoede</p>
 
             <p>Diederik Vancoppenolle, wetenschappelijk adviseur bij ’Het Agentschap Opgroeien’ vertelt dat er sinds 2019 een daling is waarbij jonge gezinnen in gunstigere omstandigheden leven dan de voorgaande jaren. Deze positieve trend blijft zich verderzetten in 2021, maar toch willen we de nadruk leggen op het feit dat er nog steeds 1 op 8 kinderen opgroeien in kansarmoede. 6% van de minderjarigen bevindt zich zelfs in een situatie van ernstige armoede met betrekking tot sociale en materiële uitsluiting (Gezinsinkomen en kansarmoede, z.d.).</p>
             <p>Voor alle recente cijfers inzage kansarmoede kan u de website van ‘Het Agentschap Opgroeien’ raadplegen <a href="https://www.opgroeien.be/cijfers-en-publicaties/gezinsinkomen-en-kansarmoede" target="_blank">(klik hier).</a> </p>
